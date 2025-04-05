027fc00aaca52d3ac9b20299a59475fcc9488fca
AtrishHub <gunjanatrish32@gmail.com>
2025-04-05 11:37:51 +0530

    User registration done

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+
+const{body}=require("express-validator");
+const userController=require('../controllers/user.controller');
+
+router.post('/register',[
+
+   body('email').isEmail().withMessage('Invalid Email'),
+   body('fullname.firstname').isLength({min:3}).withMessage('First name must be at least 3 characters long'),
+   body('password').isLength({min:6}).withMessage('Password must me at least 6 digit')
+],
+
+userController.registerUser
+
+) 
+
+//we have to check the data coming from front end is valid or not so foe this we install a package name express validator and install it 
+
+
+
+
+
+
+
+module.exports=router;
